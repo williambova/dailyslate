8294bb846eaad8f96ad6b557f5669c4364360024
William Bova <trickshottingens@gmail.com>
2026-06-09 14:11:02 -0400

    Delete lib/cn.ts

--- lib/cn.ts
+++
@@ -1,7 +0,0 @@
-import { clsx, type ClassValue } from "clsx";
-import { twMerge } from "tailwind-merge";
-
-/** Conditional + conflict-safe className helper. */
-export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs));
-}
